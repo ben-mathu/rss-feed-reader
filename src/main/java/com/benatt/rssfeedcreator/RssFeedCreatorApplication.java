be96125eeffde1c674415f932bdd18acd632b6ca
Benard Mathu <mathubenard@gmail.com>
2021-11-09 14:41:07 +0300

    Resolve conflicts with initial commit

---
+++ src/main/java/com/benatt/rssfeedcreator/RssFeedCreatorApplication.java
@@ -0,0 +1,13 @@
+package com.benatt.rssfeedcreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RssFeedCreatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RssFeedCreatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/benatt/rssfeedcreator/RssFeedCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benatt.rssfeedcreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RssFeedCreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
