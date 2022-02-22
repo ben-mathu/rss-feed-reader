3bb6ef4b0176ad014dfcacdd142d88cb0c630089
Benard Mathu <mathubenard@gmail.com>
2022-02-22 09:30:42 +0300

    update

---
+++ src/main/java/com/benatt/rssfeedcreator/services/RssFeedCreatorService.java
@@ -0,0 +1,9 @@
+package com.benatt.rssfeedcreator.services;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RssFeedCreatorService {
+
+    public void retrieveNews
+}
