332075f1898aac849cbc0d8bead8e3440190f879
Benard Mathu <mathubenard@gmail.com>
2024-07-26 08:41:30 +0300

    change to a python project

--- src/main/java/com/benatt/rssfeedcreator/services/RssFeedCreatorService.java
+++
@@ -1,9 +0,0 @@
-package com.benatt.rssfeedcreator.services;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class RssFeedCreatorService {
-
-    public void retrieveNews
-}
